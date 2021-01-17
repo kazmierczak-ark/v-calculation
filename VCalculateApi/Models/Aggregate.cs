638851d24c5a80816493b91287ccdfe5b072afa5
kazmierczak-ark <77503248+kazmierczak-ark@users.noreply.github.com>
2021-01-17 12:14:46 +0100

    initial setup of project - not cleaned (#1)
    Co-authored-by: Arkadiusz Kazmierczak <arkadiusz.kazmierczak@mobica.com>

---
+++ VCalculateApi/Models/Aggregate.cs
@@ -0,0 +1,8 @@
+namespace VCalculateApi.Models
+{
+    public class Aggregate
+    {
+        public float avg { get; set; }
+        public int sum { get; set; }
+    }
+}
---
+++ VCalculateApi/Services/StorageClient.cs
@@ -0,0 +1,47 @@
+using VCalculateApi.Interfaces;
+using InfluxDB.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace VCalculateApi.Services
+{
+    public class StorageClient : IStorageClient
+    {
+        const string bucket = "v_timeseries";
+        const string retentionPolicy = "autogen";
+
+        public async Task<(int sum, float avg)> RetrieveData(string name, int? since, int? to)
+        {
+            using (var client = InfluxDBClientFactory.CreateV1("http://localhost:8086", null, null, bucket, retentionPolicy))
+            {
+                var query = $"from(bucket: \"{bucket}/{retentionPolicy}\")";
+                query = string.Concat(query, $" |> range(start: time(v: {(since != null ? toNanoSecond(since.Value) : 0)}))");
+                if (to == null)
+                {
+                    query = string.Concat(query, $" |> range(start: time(v: {(since != null ? toNanoSecond(since.Value) : 0)}))");                
+                }
+                else
+                {
+                    query = string.Concat(query, $" |> range(start: time(v: {(since != null ? toNanoSecond(since.Value) : 0)}), stop: time(v: {toNanoSecond(to.Value)+1}))");
+                }
+                query = string.Concat(query, $" |> filter(fn: (r) => r._measurement == \"{name}\")");
+                var querySum = string.Concat(query, $" |> sum()");
+                var queryAvg = string.Concat(query, $" |> mean()");
+
+                var dataSum = await client.GetQueryApi().QueryAsync(querySum);  
+                var dataAvg = await client.GetQueryApi().QueryAsync(queryAvg);
+
+                if (dataSum.Count > 0 && dataAvg.Count > 0)
+                {
+                    return (sum: Convert.ToInt32(dataSum[0].Records[0].GetValue()), avg: Convert.ToSingle(dataAvg[0].Records[0].GetValue())); 
+                }
+                else 
+                {
+                    return (sum: 0, avg: 0); 
+                }    
+            }                
+        }
+
+        private long toNanoSecond(int time) => ((long)time)*1000000000;
+    }
+}
